69a5b7cd6865d7ab7ccb86cb04e1b022173c2876
noemie31 <99802192+noemie31@users.noreply.github.com>
2023-02-08 08:53:05 +0100

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,38 @@
+
+// L'adresse du backend local
+const BACKEND = 'http://localhost:8989';
+// Le backend est également déployé sur heroku
+// Attention : le chargement initial peut être un peu long...
+// const BACKEND = 'https://springajax.herokuapp.com';
+
+class APIError extends Error {
+  constructor(status, body) {
+    super(body.message);
+    this.status = status;
+    this.body = body;
+  }
+}
+
+/**
+ * Faire un appel AJAX avec l'API fetch
+ * Permet de récupérer erreur réseau et erreur de l'API
+ * usage :
+ * doAjaxRequest(url, options).then(showResult).catch(showError);
+ * @param {string} url L'URL de l'API
+ * @param {object} options Les options de la requête AJAX
+ * @returns {Promise} Une promesse qui sera résolue avec le résultat de l'appel AJAX
+ * @throws {APIError} Une exception qui sera levée si l'API a signalé une erreur
+ */
+async function doAjaxRequest(url, options) {
+  // On fait l'appel AJAX
+  const response = await fetch(url, options);
+  // On récupère le résultat transmis en format JSON
+  // 204 : NO-CONTENT, 404 : NOT_FOUND
+  const result = [204, 404].includes(response.status) ? null : await response.json() ;
+  // L'API a signalé une erreur, on lève une exception
+  if (!response.ok) throw new APIError(response.status, result);
+  // Tout s'est bien passé, on renvoie le résultat
+  return result;
+}
+
+export {BACKEND, doAjaxRequest, APIError};
